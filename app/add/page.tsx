911ad988a94a8917286ca88229e0158420fed8e9
Ako Mawlood <ako.mawlood22@gmail.com>
2023-09-20 05:06:03 +0300

    added some styleing

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+import img from "public/pngwing.com.png"
+import Image from 'next/image'
 import { useState} from "react"
 import { useRouter } from 'next/navigation';
 const Add = () => {
@@ -52,26 +54,31 @@ const Add = () => {
        
   return (
     <> {error && <h1 className=" m-36 text-purple-500 text-xl font-bold">{error}</h1>}
-    <div className=" flex-col items-center flex justify-center   hhh  ">
-      <form onSubmit={handleSubmit} className="text-white text-2xl bg-purple-500 mt-12  rounded-3xl flex justify-evenly items-center flex-col add h-3/4" >
+    <div className=" flex items-center justify-center  hhh  ">
+       
+      <form onSubmit={handleSubmit} className="text-purple-500 shadow-md shadow-black text-2xl border-solid border-purple-400 border-4 bg-purple-100 mt-20 mr-36 rounded-3xl flex justify-evenly items-center flex-col add h-3/4 " >
          <div className="flex flex-col">
           <label className=" ">Name:</label>
-          <input  required className="w-96 p-4 rounded-lg text-purple-400" onChange={(e)=>setName(e.target.value)} placeholder="Alexander" type="text" value={name}/>
+          <input  required className="w-96 p-4 border-solid border-purple-500 border-4 rounded-lg text-purple-400" onChange={(e)=>setName(e.target.value)} placeholder="Alexander" type="text" value={name}/>
         </div>
         <div className="flex flex-col">
           <label className="">Title:</label>
-        <input required className="w-96 text-purple-400 rounded-lg p-4" onChange={(e)=>setTitle(e.target.value)} placeholder="Climate Change" type="text" value={title}/>
+        <input required className="w-96 text-purple-400 rounded-lg p-4 border-solid border-4 border-purple-500" onChange={(e)=>setTitle(e.target.value)} placeholder="Climate Change" type="text" value={title}/>
         </div>
         <div className="flex flex-col ">
         <label className="">Body:</label>
-        <textarea required placeholder="My opinion about..." className="w-96 text-purple-500 p-4 rounded-lg"onChange={(e)=>setBody(e.target.value)} value={body}></textarea>
+        <textarea required placeholder="My opinion about..." className="w-96 text-purple-500 p-4 rounded-lg border-solid border-4 border-purple-500 "onChange={(e)=>setBody(e.target.value)} value={body}></textarea>
         </div>
        
         
         
-        <input className=" cursor-pointer bg-white text-purple-500 py-3 px-7 rounded-lg" type="submit" value="Publish"/>
+        <input className=" cursor-pointer p-4 ml-2 rounded-xl font-bold text-lg text-white bg-purple-500 bg-perpule-600 text-center" type="submit" value="Publish"/>
+
       </form>
+
     </div>
+    <Image width={700} height={550} alt='photo' src={img}  quality={100}  className=' butter'/>
+   
  </> 
  )
 }
